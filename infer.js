857309d892245ef9b9ef5184a01b577cea86ae12
Dan Tao <daniel.tao@gmail.com>
2014-07-17 09:47:07 -0700

    refactored node type definitions into generated constructors

--- infer.js
+++ infer.js
@@ -1,9 +1,11 @@
 var esprima = require('esprima'),
+    Node = require('./lib/node'),
     Identifier = require('./lib/identifier'),
     Scope = require('./lib/scope');
 
 function infer(js) {
-  return new Scope(null, esprima.parse(js));
+  var ast = esprima.parse(js);
+  return new Scope(null, Node.create(ast));
 }
 
 infer.Identifier = Identifier;
